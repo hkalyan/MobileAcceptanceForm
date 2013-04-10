e17aa362878fce541075d9869cddbefd695c3561
Hari Phaneendra <hdattada@gmail.com>
2013-04-09 22:20:49 -0400

    Added Banner and click events

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
 	    // call the tablesorter plugin 
 		$("#maintable").tablesorter(); 
 
-		$(".image").click(function()
+		$(".image").click(function(eventData)
 		{
+			var eventid = $(this).attr('id');
 			console.log("Clicked");
 			$( "#dialog-modal" ).dialog({
 				modal: true,
@@ -24,7 +25,9 @@
 					open: function(event, ui)
 					{
 						var img = $('<img ></img>');
-						img.attr('src','images/userinformationinputzoom.png');
+						var imgsrc = 'images/'+eventid+'.png';
+						//img.attr('src','images/userinformationinputzoom.png');
+						img.attr('src',imgsrc);
 						$(this).html(img);
 					
 						//$(textarea).setCode('<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>');
@@ -38,6 +41,10 @@
 </head>
 
 <body>
+<div style="top-margin:10px">
+<img width="100%" src="http://i.imgur.com/CesevhY.png" alt='Create your own banner at mybannermaker.com!' border=0 /></a><br>
+
+</div>
 <div id="dialog-modal" title="Wireframe" style="display: none;text-align:center"></div>
 <table id="maintable" cellspacing="1" class="tablesorter" >
 <thead>
@@ -61,7 +68,7 @@
 	</thead>
 	<tbody>
 	<tr id="odd">
-		<td class="image" align="center"><img alt="" src="images/appicon.png"></img></td>
+		<td class="image" id="appicon" align="center"><img alt="" src="images/appicon.png"></img></td>
 		<td>IMAGE</td>
 		<td>First Time User Screen</td>
 		<td><select>
@@ -72,7 +79,7 @@
 		<td><textarea rows="5" cols="50"></textarea></td>
 	</tr>
 	<tr>
-		<td class="image"><img alt="" src="images/va_logo.png" ></img></td>
+		<td class="image" id="va_logo" ><img alt="" src="images/va_logo.png" ></img></td>
 		<td>IMAGE</td>
 		<td>First Time User Screen</td>
 		<td><select>
@@ -95,7 +102,7 @@
 		<td><textarea rows="5" cols="50"></textarea></td>
 	</tr>
 	<tr>
-		<td class="image"><img id="userinput" height="63" width="66" alt="" src="images/userinformationinputzoom.png" ></img></td>
+		<td class="image" id="userinformationinputzoom"><img  height="63" width="66" alt="" src="images/userinformationinputzoom.png" ></img></td>
 		<td>Input Fields</td>
 		<td>First Time User Screen</td>
 		<td><select>
@@ -106,7 +113,7 @@
 		<td><textarea rows="5" cols="50"></textarea></td>
 	</tr>
 	<tr>
-		<td class ="image"><img id="continuebtn" height="63" width="66" alt="" src="images/userinformationinputzoom.png" ></img></td>
+		<td class ="image" id="continuebtn" ><img height="63" width="66" alt="" src="images/userinformationinputzoom.png" ></img></td>
 		<td>Input Fields</td>
 		<td>First Time User Screen</td>
 		<td><select>
